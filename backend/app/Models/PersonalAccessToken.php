a48cbe6ab92bdefb46190e83d63108bfe0463672
Sashank206 <sashank8885@gmail.com>
2026-05-27 00:42:06 +0530

    Fixed HTTPS redirect issue

--- backend/app/Models/PersonalAccessToken.php
+++ backend/app/Models/PersonalAccessToken.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-use MongoDB\Laravel\Eloquent\DocumentModel;
 use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
+use MongoDB\Laravel\Eloquent\DocumentModel;
 
 class PersonalAccessToken extends SanctumPersonalAccessToken
 {
     use DocumentModel;
 
     protected $connection = 'mongodb';
-    protected $collection = 'personal_access_tokens';
+    protected $table = 'personal_access_tokens';
     protected $primaryKey = '_id';
     protected $keyType = 'string';
 }
