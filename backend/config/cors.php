a343703f81e01b43d058609586b81555f03ad374
Sashank206 <sashank8885@gmail.com>
2026-05-27 00:54:24 +0530

    Fixed final CORS preflight

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,5 +1,4 @@
-
-<?php
+<!-- <?php
 
 return [
 
@@ -7,7 +6,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://path-mitra.vercel.app',
+        'http://localhost:5173',
+    ],
+
+
 
     'allowed_origins_patterns' => [],
 
